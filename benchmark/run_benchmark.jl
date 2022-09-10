8fcc04cd9b4b6869888d0be203801d31d1a069c1
junder873 <junder873@gmail.com>
2022-09-10 13:53:22 -0400

    update benchmark timings

--- benchmark/run_benchmark.jl
+++ benchmark/run_benchmark.jl
@@ -1,4 +1,4 @@
-using DataFrames, CSV, DataFramesMeta, Dates, BenchmarkTools, Cthulhu, SparseArrays, StaticArrays, LinearAlgebra
+using DataFrames, CSV, DataFramesMeta, Dates, BenchmarkTools, SparseArrays, StaticArrays, LinearAlgebra
 using Revise
 using AbnormalReturns
 
@@ -10,12 +10,10 @@ df_events = CSV.File(joinpath("data", "event_dates.csv")) |> DataFrame
 
 ##
 @time data = MarketData(df_mkt, df_firm; id_col=:firm_id, valuecols_firms=[:ret])
-# First run R5 3600: 20.229416 seconds (34.98 M allocations: 13.211 GiB, 12.81% gc time, 64.56% compilation time)
-# Second run R5 3600: 8.131775 seconds (681.52 k allocations: 11.431 GiB, 26.59% gc time, 0.06% compilation time)
 # First run R7 5700X: 16.141040 seconds (35.39 M allocations: 13.239 GiB, 14.37% gc time, 56.56% compilation time)
 # Second run R7 5700X: 5.998991 seconds (547.61 k allocations: 11.431 GiB, 29.92% gc time)
-# First run i7 6700: 30.367487 seconds (29.85 M allocations: 12.966 GiB, 40.87% gc time, 38.86% compilation time)
-# Second run i7 6700: 10.302273 seconds (688.03 k allocations: 11.436 GiB, 22.65% gc time)
+# First run i7 6700: 31.045712 seconds (35.03 M allocations: 13.222 GiB, 32.21% gc time, 64.39% compilation time)
+# Second run i7 6700: 11.217124 seconds (537.50 k allocations: 11.431 GiB, 25.19% gc time)
 
 ##
 df_firm = nothing
@@ -39,12 +37,10 @@ GC.gc()
         :beta = beta(:reg_mkt),
     )
 end
-# First run R5 3600: 12.886998 seconds (35.27 M allocations: 4.308 GiB, 13.43% gc time, 70.63% compilation time)
-# Second run R5 3600: 3.537313 seconds (12.73 M allocations: 3.092 GiB, 24.15% gc time, 2.50% compilation time)
 # First run R7 5700X: 9.017057 seconds (30.39 M allocations: 3.184 GiB, 10.33% gc time, 85.86% compilation time)
 # Second run R7 5700X: 1.453299 seconds (7.18 M allocations: 1.988 GiB, 13.25% gc time, 5.04% compilation time)
-# First run i7 6700: 17.520028 seconds (26.97 M allocations: 4.145 GiB, 28.89% gc time, 77.12% compilation time)
-# Second run i7 6700: 5.772432 seconds (4.73 M allocations: 2.944 GiB, 44.27% gc time, 1.59% compilation time)
+# First run i7 6700: 28.656179 seconds (33.49 M allocations: 3.360 GiB, 53.20% gc time, 90.52% compilation time)
+# Second run i7 6700: 3.095861 seconds (7.13 M allocations: 1.985 GiB, 2.87% compilation time)
 
 ##
 
